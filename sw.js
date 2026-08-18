13c8b05c1f4c5488800a4729ad93784faa4885c0
aliriojuniorvet <aliriojunior.vet@gmail.com>
2026-08-17 21:10:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "equibr-shell-v2";
-const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-512.png"];
+const CACHE_NAME = "equibr-shell-v3";
+const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
